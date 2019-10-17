4741a0ed68dad32f84e7ea4522ad3bc5227835f2
Aleksandar Toprek <toprek.aleksandar@gmail.com>
2019-10-17 17:56:59 +0200

    Add functionality to make reservation

--- src/TurističkaAgencija/Controllers/RezervacijaController.cs
+++ src/TurističkaAgencija/Controllers/RezervacijaController.cs
@@ -19,10 +19,30 @@ namespace TurističkaAgencija.Controllers
         }
 
         // GET: Rezervacija
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? ponudaId)
         {
-            var turistickaAgencijaContext = _context.Rezervacija.Include(r => r.Korisnik).Include(r => r.Ponuda);
+            if(ponudaId == null)
+            {
+                return NotFound();
+            }
+            var turistickaAgencijaContext = _context.Rezervacija
+                .Include(r => r.Korisnik)
+                .Include(r => r.Ponuda)
+                .Where(r => r.PonudaId == ponudaId);
+            if(turistickaAgencijaContext == null)
+            {
+                return NotFound();
+            }
             return View(await turistickaAgencijaContext.ToListAsync().ConfigureAwait(false));
+        }
+        public async Task<IActionResult> ExistingUserPartial()
+        {
+            ViewData["KorisnikId"] = new SelectList(_context.Korisnik, "Id", "Ime");
+            return PartialView();
+        }
+        public async Task<IActionResult> NewUserPartial()
+        {
+            return PartialView();
         }
 
         // GET: Rezervacija/Details/5
@@ -47,11 +67,19 @@ namespace TurističkaAgencija.Controllers
         }
 
         // GET: Rezervacija/Create
-        public IActionResult Create()
+        public IActionResult Create(int? ponudaId)
         {
+            if(ponudaId == null)
+            {
+                return NotFound();
+            }
+            var rezervacijaContext = _context.Rezervacija
+                .Include(k => k.Ponuda)
+                .Where(x => x.PonudaId == ponudaId);
+
             ViewData["KorisnikId"] = new SelectList(_context.Korisnik, "Id", "Ime");
-            ViewData["PonudaId"] = new SelectList(_context.Ponuda, "Id", "Naziv");
-            return View();
+
+            return View(rezervacijaContext.FirstOrDefault());
         }
 
         // POST: Rezervacija/Create
@@ -59,20 +87,48 @@ namespace TurističkaAgencija.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,PonudaId,KorisnikId,DatumRezervacije,Iznos")] Rezervacija rezervacija)
+        public async Task<IActionResult> Create(int ponudaId, Rezervacija rezervacija)
         {
             if(rezervacija == null)
             {
                 return NotFound();
             }
+
+            var ponuda = await _context.Ponuda.FindAsync(rezervacija.PonudaId).ConfigureAwait(false);
+            if(ponuda == null)
+            {
+                return NotFound();
+            }
+
             if (ModelState.IsValid)
             {
-                _context.Add(rezervacija);
-                await _context.SaveChangesAsync().ConfigureAwait(false);
-                return RedirectToAction(nameof(Index));
+                if (rezervacija.Korisnik != null)
+                {
+                    _context.Add(rezervacija.Korisnik);
+                    await _context.SaveChangesAsync().ConfigureAwait(false);
+
+                    var noviKorisnik = _context.Korisnik
+                        .OrderByDescending(x => x.Id)
+                        .FirstOrDefault();
+
+                    rezervacija.KorisnikId = noviKorisnik.Id;
+                    rezervacija.Iznos = ponuda.Cijena;
+                    rezervacija.DatumRezervacije = DateTime.Now;
+
+                    _context.Add(rezervacija);
+                    await _context.SaveChangesAsync().ConfigureAwait(false);
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    rezervacija.Iznos = ponuda.Cijena;
+                    rezervacija.DatumRezervacije = DateTime.Now;
+                    _context.Add(rezervacija);
+                    await _context.SaveChangesAsync().ConfigureAwait(false);
+                    return RedirectToAction(nameof(Index));
+                }
             }
             ViewData["KorisnikId"] = new SelectList(_context.Korisnik, "Id", "Ime", rezervacija.KorisnikId);
-            ViewData["PonudaId"] = new SelectList(_context.Ponuda, "Id", "Naziv", rezervacija.PonudaId);
             return View(rezervacija);
         }
 
